b2226c9ea99adeb392bbc1230c0b9b08bdb9522d
Andrey Kochergin <Andrey.Kochergin@nih.gov>
2022-04-14 19:06:23 -0400

    SRAB-100: added usage to run sh

--- toolbox/workflow/run_corona_pacbio.sh
+++ toolbox/workflow/run_corona_pacbio.sh
@@ -1,16 +1,29 @@
 #!/bin/bash -eu
 
 dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+progname=`basename $0`
 
 acc=
 cpu=6
 cfg=corona_pacbio.config.yaml
 
+function usage {
+cat <<EOF
+--USAGE:
+  $dir/$progname --acc <accession> [--cfg <config yaml>] [--cpu <number of cores>]
+  where:
+    --acc <acession>        : required
+    --cpu <number of cores> : optional, default = $cpu
+    --cfg <path to config>  : optional, default = $cfg
+EOF
+    exit 1
+}
+
 while (( $# > 0 )); do
   case "x$1" in
     x--cfg ) cfg=$2; shift;;
-    x--acc ) acc=$2;    shift;;
-    x--cpu ) cpu=$2;    shift;;
+    x--acc ) acc=$2; shift;;
+    x--cpu ) cpu=$2; shift;;
   esac
   shift
 done
