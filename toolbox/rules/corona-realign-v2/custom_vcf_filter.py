ec6f1ac265ed44cae6490f1229d6969bc12a8474
Andrey Kochergin <Andrey.Kochergin@nih.gov>
2022-05-10 19:24:37 -0400

    SRAB-309: custom filter to wf

---
+++ toolbox/rules/corona-realign-v2/custom_vcf_filter.py
@@ -0,0 +1,192 @@
+import argparse
+import logging.handlers
+import sys
+from logging import _nameToLevel
+from typing import Dict
+import json
+import numpy as np
+import collections
+
+"""
+custom VCF filter, which could not be or hard to implement using GATK VariantFiltration or Selection. 
+see SRAB-308
+    && $ad_alt / $DP_unfilt >= 0.15 && $dep >= 10
+        ignoring those variants with AF < 0.15;  
+        "ad_alt" for ALT allele counts from FORMAT field, 
+        "DP_unfilt" for DP from INFO field;  
+        "dep" for the coverage of this variant POS from bam_genomecoverage
+    && $dep_min_next10bps >= 10
+        ignoring this variant if the minimum coverage of NEXT 10bps after this POS is 10, 
+        for getting rid of some bad calls from read ends
+    && $dp_ori / $DP_unfilt >= 0.5 && $dep / $dp_ori >= 0.5
+        trying to filter out bad calls from regions with too much differences between DP from INFO field (DP_unfilt), 
+        "dp_ori" (DP from FORMAT), and "dep" (DP from bam_genomecoverage).
+"""
+
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+else:
+    logger = None
+
+
+class VCF:
+    row = collections.namedtuple('row', 'pos ref alt qual info formats')
+
+    def __init__(self, vcf_file, depth_file):
+        self.meta = collections.defaultdict(list)
+        self.locations = []
+        self.header = []
+        self.format = None
+        self.columns = None
+        self.vcf = None
+
+        self.vcf_file = vcf_file
+        self.depth_file = depth_file
+
+    def __exit__(self, *args): ...
+
+    def __enter__(self):
+        self.load_depth(self.depth_file)
+        self.load_vcf(self.vcf_file)
+        return self
+
+    def parse(self):
+        def _map(pair):
+            if len(pair) == 1:
+                pair.append(True)
+            return pair
+        for loc in self.locations:
+            yield self.row._make([int(loc.pos),
+                                  loc.ref,
+                                  loc.alt,
+                                  loc.qual,
+                                  dict([_map(a.split('=')) for a in loc.info.split(';')]),
+                                  [self.format._make(f.split(':')) for f in loc[9:]]]), loc
+
+    def get_min_depth_4_window(self, pos, window = 10):
+        return self.depth[pos - 1:pos + window - 1].min()
+
+    def get_depth_window(self, pos, window = 10):
+        return self.depth[pos - 1:pos + window - 1].tolist()
+
+    def get_depth(self, pos):
+        return self.depth[pos - 1]
+
+    def load_depth(self, depth):
+        self.depth = np.loadtxt(depth, delimiter='\t', usecols=[2])
+
+    def load_vcf(self, vcf):
+        with open(vcf) as fp:
+            for e, l in enumerate(fp):
+
+                l = l.rstrip()
+                if self.vcf is None:
+                    if l.startswith('##fileformat='):
+                        self.vcf = l.split('=')[1]
+                        self.header.append(l)
+                        continue
+                    else:
+                        raise Exception('It does not look like VCF file')
+                elif l.startswith('##'):
+                    self.header.append(l)
+                    pair = l[2:].split('=', 1)
+                    if pair[0] in ['INFO', 'FORMAT', 'FILTER']:
+                        self.meta[pair[0]].append(pair[1][4:-1].split(',')[0])
+                    else:
+                        self.meta[pair[0]]=pair[1]
+                    continue
+                elif l.startswith('#'):
+                    self.header.append(l)
+                    cols = ' '.join([c.lower() if c.isalnum() else os.path.basename(c).split('.')[0].lower() for c in l[1:].split('\t')])
+                    self.columns = collections.namedtuple('vcf', cols)
+                    continue
+                else:
+                    self.locations.append(self.columns._make(l.rstrip().split('\t')))
+
+        # get first line to define FORMAT tuple
+        self.format = collections.namedtuple('format', ' '.join([c for c in self.locations[0].format.split(':')]))
+        # apparently INFO is not consistent thus cannot use named tuple
+
+input_description = ('vcf:path', 'coverage:path')
+output_description = ('vcf:path', )
+
+
+def run(_input: Dict, _output: Dict, _config: Dict, _rule: Dict) -> int:
+    """
+    :param _input:
+    :param _output:
+    :param _config:
+    :param _rule:
+    :return:
+    """
+
+    snps = 0
+    with VCF(_input["vcf"], _input["coverage"]) as vcf:
+        with open(_output["vcf"], 'wt') as o_vcf:
+            for h in vcf.header:
+                o_vcf.write(h)
+                o_vcf.write('\n')
+            for v, r in vcf.parse():
+                ad_alt = int(v.formats[0].AD.split(',')[1]) #  for ALT allele counts from FORMAT field
+                DP_unfilt = int(v.info['DP'])               #  for DP from INFO field
+                dp_ori = int(v.formats[0].DP)               #  DP from FORMAT
+
+                del_sz = len(v.ref) - len(v.alt)
+                if del_sz > 0:
+                    pos_d = v.pos + del_sz + 1
+                else:
+                    pos_d = v.pos + len(v.ref) + 1
+                dep = vcf.get_depth(v.pos)
+                dep_min_next10bps = vcf.get_min_depth_4_window(pos_d)
+
+                if v.alt != '*' and dep_min_next10bps >= 10 \
+                        and dp_ori / DP_unfilt >= 0.5 \
+                        and dep / dp_ori >= 0.5 \
+                        and ad_alt / DP_unfilt >= 0.15 \
+                        and dep >= 10:
+                    o_vcf.write('\t'.join(r))
+                    o_vcf.write('\n')
+                    snps += 1
+                elif logger:
+                    logger.warning(f"filtered {v}")
+                    logger.warning(f"\tdep_min_next10bps >= 10 {dep_min_next10bps >= 10}, pos_d = {pos_d}, depth = {vcf.get_depth_window(pos_d)}")
+                    logger.warning(f"\tdp_ori / DP_unfilt >= 0.5 {dp_ori / DP_unfilt >= 0.5}")
+                    logger.warning(f"\tdep / dp_ori >= 0.5 {dep / dp_ori >= 0.5}")
+                    logger.warning(f"\tad_alt / DP_unfilt >= 0.15 {ad_alt / DP_unfilt >= 0.15}")
+                    logger.warning(f"\tdep >= 10 {dep >= 10}")
+    if not snps:
+        sys.stderr.write('no-SNPs')
+        return 1
+    return 0
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--c',
+                        help='path to depth file, built using bedtools genomecov -ibam $bam -g $ref -d > $depth',
+                        required=True)
+    parser.add_argument('--i',
+                        help='path to input VCF file',
+                        required=True)
+    parser.add_argument('--o',
+                        help='path to output VCF file',
+                        required=True)
+
+    parser.add_argument('--log-level', help='log level', choices=list(_nameToLevel.keys()),
+                        default='INFO')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level,
+                        format='%(asctime)s.%(msecs)03d %(module)s..%(funcName)s [%(levelname)s] %(message)s',
+                        datefmt='%m-%d-%Y %H:%M:%S')
+
+    run(_input={"vcf": args.i, "coverage": args.c},
+        _output={"vcf": args.o},
+        _config={}, _rule={})
+
+
+if __name__ == '__main__':
+    # for testing purposes
+    main()
